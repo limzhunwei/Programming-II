354e62b2aaf231810ffb0eb97430e0fabebcb996
Lim Zhun Wei <72724444+limzhunwei@users.noreply.github.com>
2021-03-30 21:08:17 +0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,21 @@
+package Example3;
+
+public class Dog {
+	
+	String breed;
+	String colour;
+	String size;
+	
+	void Run() {
+		System.out.println("Dog is running");
+	}
+	
+	void Jump() {
+		System.out.println("Dog is jumping");
+	}
+	
+	void Sit() {
+		System.out.println("Dog is Sitting");
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+package Example3;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Dog d = new Dog();
+		d.Jump();
+		d.Run();
+		d.Sit();
+		
+		Motorcycle m = new Motorcycle();
+		m.Speed();
+		m.Break();
+		m.Honk();
+		
+		
+	}
+
+}
