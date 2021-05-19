6fefdaf6b20d5008ebf08fea0fd44718d1c407e9
Lim Zhun Wei <72724444+limzhunwei@users.noreply.github.com>
2021-05-19 14:06:48 +0800

    Delete Abstraction directory

--- Abstraction/Abstraction/Price.java
+++
@@ -1,9 +0,0 @@
-package Abstraction;
-
-public interface Price {
-	
-	double totalPrice(double pp, double qq);
-	
-	double totalPrice(double pp, double qq, double d);
-
-}
--- Abstraction/Total_Price.java
+++
@@ -1,9 +0,0 @@
-package Abstraction;
-
-public interface Total_Price {
-	
-	double totalPrice(double pp, double qq);
-	
-	double totalPrice(double pp, double qq, double d);
-
-}
